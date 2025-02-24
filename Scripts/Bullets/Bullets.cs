2708fe0d0e8a70b0bc41cba94d839564b4922ebb
Abhyu <abhyudayav@gmail.com>
2025-02-24 17:33:36 -0600

    Made new Bullets class with properties

--- Scripts/Bullets/Bullets.cs
+++ Scripts/Bullets/Bullets.cs
@@ -1,8 +1,16 @@
 using Godot;
 using System;
 
-public partial class NewScript : Node
+public partial class Bullets : Node
 {
+	Vector2 movement_vector; // Movement Direction
+	Vector2 current_postion; // Current Position
+	float lifetime = 0, // How long the bullet has existed
+		  speed = 0, // Speed of the bullet (duh)
+		  image_offset = 0, // Used for switching bullet textures (IDK if we will do this but putting the property here anyways)
+		  animation_lifetime = 0; // Keeps track of the length since the last switch
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
